11080e4e66c1b566ac1e14d890b5a4ac00995a90
jafayer <github@fayer.me>
2023-01-23 14:20:02 -0500

    Add server-side connection logic

--- src/server/connect.ts
+++ src/server/connect.ts
@@ -1,7 +1,15 @@
-import * as HID from "node-hid";
-import { HIDFilters } from "../utils/consts";
+import * as HID from 'node-hid';
+import { HIDFilters } from '../utils/consts';
 
 export default function connect() {
-    const device = new HID.HID(HIDFilters[0].vendorId, HIDFilters[0].productId) || new HID.HID(HIDFilters[1].vendorId, HIDFilters[1].productId);
+  const devices = HID.devices();
+  const deviceInfo = devices.find((d) => {
+    return HIDFilters.some((params) => d.vendorId === params.vendorId && d.productId === params.productId);
+  });
+  if (deviceInfo && deviceInfo.path) {
+    const device = new HID.HID(deviceInfo.path);
     return device;
+  } else {
+    return undefined;
+  }
 }
