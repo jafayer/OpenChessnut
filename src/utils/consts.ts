450158b91325e7c376ea159bc3274a30e31cc1db
jafayer <github@fayer.me>
2023-01-23 14:20:42 -0500

    Format consts and add empty types file

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,5 +1,4 @@
-
 export const HIDFilters = [
-    { vendorId: 0x2d80, productId: 0x8001 },
-    { vendorId: 0x2d80, productId: 0x8002 },
-]
+  { vendorId: 0x2d80, productId: 0x8001 },
+  { vendorId: 0x2d80, productId: 0x8002 },
+];
