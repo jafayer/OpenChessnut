cb079d31a53fdedbc78d7429691c3c7fa18653fb
jafayer <github@fayer.me>
2023-01-23 14:21:47 -0500

    re-add web-side temporary start report consts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,3 +2,5 @@ export const HIDFilters = [
   { vendorId: 0x2d80, productId: 0x8001 },
   { vendorId: 0x2d80, productId: 0x8002 },
 ];
+
+export const start: [number, Uint8Array] = [0x21, new Uint8Array([0x01, 0x00])];
