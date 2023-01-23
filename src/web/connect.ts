6cb00b7f2ce09f5a85669d09149706b6ed98f39d
jafayer <github@fayer.me>
2023-01-23 14:19:22 -0500

    Format web connection script

--- src/web/connect.ts
+++ src/web/connect.ts
@@ -1,24 +1,24 @@
-import { HIDFilters, start } from "../utils/consts";
+import { HIDFilters, start } from '../utils/consts';
 
 export async function connectHID(callback?: CallableFunction) {
-    const [device] = await navigator.hid.requestDevice({
-        filters: HIDFilters,
-    });
+  const [device] = await navigator.hid.requestDevice({
+    filters: HIDFilters,
+  });
 
-    if(device) {
-        await device.open();
-        await device.sendReport(start[0], start[1]);
+  if (device) {
+    await device.open();
+    await device.sendReport(start[0], start[1]);
 
-        if(callback) {
-            callback(device);
-        }
-    
-        return device;
-    } else {
-        if(callback) {
-            callback(undefined);
-        }
-
-        return undefined;
+    if (callback) {
+      callback(device);
     }
+
+    return device;
+  } else {
+    if (callback) {
+      callback(undefined);
+    }
+
+    return undefined;
+  }
 }
